9aeabb57d82510ad3a043309ff162197340a27e5
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2024-05-04 22:47:12 +0700

    commit

---
+++ src/main/java/com/exampl/clothstoremvc/ClothstoremvcApplication.java
@@ -0,0 +1,13 @@
+package com.exampl.clothstoremvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothstoremvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothstoremvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampl/clothstoremvc/ClothstoremvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampl.clothstoremvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothstoremvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
